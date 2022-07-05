8978ed6abab85a61c1437884b9d9ea83d1f9f2b4
Lucas Coelho <l.coelhooliveira@gmail.com>
2022-07-05 11:48:25 -0300

    Update package

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
 
-import theme from './theme';
 import Router from './config/Router';
 
 function App() {
-  return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Router />
-    </ChakraProvider>
-  );
+  return <Router />;
 }
 
 export default App;
--- src/theme/colors.js
+++
@@ -1,9 +0,0 @@
-const brandPallete = {
-  500: '#2E3192',
-};
-
-const colors = {
-  brand: brandPallete,
-};
-
-export default colors;
